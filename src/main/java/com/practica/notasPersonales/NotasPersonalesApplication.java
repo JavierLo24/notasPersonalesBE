03b9d54d7f3666164b05f5e143c51f65ac7dcd0d
JavierLo24 <javierandres.123@hotmail.com>
2025-06-03 10:06:37 -0500

    Init Commit

---
+++ src/main/java/com/practica/notasPersonales/NotasPersonalesApplication.java
@@ -0,0 +1,13 @@
+package com.practica.notasPersonales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasPersonalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotasPersonalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/notasPersonales/NotasPersonalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.notasPersonales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasPersonalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
